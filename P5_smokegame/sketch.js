732331d4ff07389c732fea921859762c6ad9715d
Konoha211 <100297520+Konoha211@users.noreply.github.com>
2024-09-09 10:16:53 -0400

    game demo
    365365365365365365365

--- P5_smokegame/sketch.js
+++ P5_smokegame/sketch.js
@@ -1,7 +1,72 @@
+let squares = [];
+let currentSquare = 0;
+let gameWon = false;
+let gameLost = false;
+let canInteract = true;
+let lastObstacleTime = 0;
+let obstacleInterval = 0;
+
+function preload() {
+  bgm = loadSound("365_partygirl.mp3");
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(500, 100);
+  for (let i = 0; i < 10; i++) {
+    squares.push(false);
+  }
+  createButton('bumpin that').mousePressed(interact);
+  bgm.loop();
 }
 
 function draw() {
-  // put drawing code here
+  if (gameWon) {
+    background('#8ACE00');
+  } else if (gameLost) {
+    background(255, 0, 0);
+  } else {
+    background(0);
+    handleObstacles();
+    drawSquares();
+  }
+}
+
+function drawSquares() {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i]) {
+      fill('#8ACE00');
+    } else {
+      fill(0);
+    }
+    stroke(255);
+    rect(i * 50 + 5, 25, 40, 40);
+  }
+}
+
+function interact() {
+  if (!canInteract) {
+    gameLost = true;
+    return;
+  }
+  
+  if (currentSquare < squares.length) {
+    squares[currentSquare] = true;
+    currentSquare++;
+    
+    if (currentSquare === squares.length) {
+      gameWon = true;
+    }
+  }
+}
+
+function handleObstacles() {
+  if (millis() - lastObstacleTime > obstacleInterval) {
+    canInteract = !canInteract;
+    lastObstacleTime = millis();
+    obstacleInterval = random(100, 2000);
+  }
+  
+  if (!canInteract) {
+    background(255, 0, 0);
+  }
 }
